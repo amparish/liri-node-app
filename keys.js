bd4174e416406cd6edb96210b3f59d21ce09e76d
amparish <alexmparish@gmail.com>
2017-06-19 01:54:19 -0500

    created files and added twitter API key

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'nSR73HDDITs1781uXrao7Algl',
+  consumer_secret: 'Nx6f0YscxTMjB2ovU4jNsJdgwdT0k4tAy3SCtwnYCQqW3sv3Sz',
+  access_token_key: '14967834-i6kekCg6U8MlmWh23yoKbtRqw7hIsJhW7hBoAwhCs',
+  access_token_secret: 'YUvCDNdLRZjHGvhciO7To8kCV7xd5P6y5xYAmdci9FFg0',
+}
---
+++ liri.js
@@ -0,0 +1,4 @@
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var request = require("request");
